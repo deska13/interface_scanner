c5e158ff05f83856d22193d2009dc23d01434ab7
deska13 <deskakuzmin1999@yandex.ru>
2022-04-11 23:26:21 +0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -7,13 +7,14 @@ import OrdersByTransportIdPages from "../pages/OrdersByTransportIdPages";
 
 
 export const privateRoutes = [
+    
+]
+
+export const publicRoutes = [
+    {path: '/login', component: Login, exact:true},
     {path: '/about', component: About, exact: true},
     {path: '/clients', component: Clients, exact: true},
     {path: '/clients/:id', component: ClientIdPages, exact: true},
     {path: '/transports/:id_transport', component: TransportIdPages, exact: true},
     {path: '/orders/:id_transport', component: OrdersByTransportIdPages, exact: true},
-]
-
-export const publicRoutes = [
-    {path: '/login', component: Login, exact:true}
 ]
